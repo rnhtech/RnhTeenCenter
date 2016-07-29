029008b946059b570b65274a92ea1eafe36e1045
Rnh Tech <rnhtech@riverdaleonline.org>
2016-07-29 17:58:03 -0400

    Updated home page

--- client/main.js
+++ client/main.js
@@ -1,5 +1,32 @@
 import angular from 'angular';
 import angularMeteor from 'angular-meteor';
-import 'bootstrap/dist/css/bootstrap.css';
+// import 'bootstrap/dist/css/bootstrap.css';
+// import fullpage from 'fullpage';
 
 import { name as TeenProgram } from '../imports/ui/components/teenprogram/teenprogram';
+
+// //initialize fullpage.js
+// $(document).ready(() => {
+//   $("#fullpage").fullpage({
+//     // verticalCentered: false,
+//     // css3: true,
+//     // scrollingSpeed: 700,
+//     // autoScrolling: true,
+//     // fitToSection: true,
+//     // fitToSectionDelay: 1000,
+//     // scrollBar: false,
+//     // easing: 'easeInOutCubic',
+//     // easingcss3: 'ease',
+//     // loopBottom: false,
+//     // loopTop: false,
+//     // loopHorizontal: true,
+//     // continuousVertical: false,
+//     // normalScrollElements: '#element1, .element2',
+//     // scrollOverflow: false,
+//     // scrollOverflowOptions: null,
+//     // // touchSensitivity: 15,
+//     // normalScrollElementTouchThreshold: 5,
+//     // bigSectionsDestination: null,
+//
+//   });
+// });
